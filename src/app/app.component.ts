717b1af9565f4e3913e7f963c98cd5af95035b7c
bhuvaneshs <blazingbhuvan@gmail.com>
2019-09-29 21:02:00 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,32 @@
-import { Component } from '@angular/core';
-import { of } from 'rxjs';
+import { Component } from "@angular/core";
+import { Observable, of, from } from "rxjs";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular';
-
+export class AppComponent {
+  name = "Angular";
+  datas: any[] = ["data1", "data2", 2];
   // RxJS v6+
-ngOnInit(){
-//emits values of any type
-const source = of({ name: 'Brian' }, [1, 2, 3], function hello() {
-  return 'Hello';
-});
-//output: {name: 'Brian'}, [1,2,3], function hello() { return 'Hello' }
-const subscribe = source.subscribe(val => console.log(val));
-}
+  ngOnInit() {
+    const observer = {
+      next: value => console.log(`Emitted Value : ${value}`),
+      error: err => console.log(`Error occurred : ${err}`),
+      complete: () => console.log("Action completed")
+    };
+
+    const stream = new Observable(observer => {
+      for (let data of this.datas) {
+        if (data == 2) {
+          observer.error("unknown value");
+        }
+        observer.next(data);
+      }
+      observer.complete();
+    });
+
+    stream.subscribe(observer);
+  }
 }
